b14de4ae409e94989af8ca845652ba0c51577519
superzdd <superzdd@sina.com>
2020-10-20 12:00:52 +0800

    add env

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # frontend-practice
 > 自己的一些关于前端知识点的整理集合，方便自己写过忘了之后可以回看
 
-
-
 #### p5js
 
 基于p5的一些实例
 
 - noise系列：关于柏林噪声的各种图像研究
 
-
-
 #### vtour
 
 基于krpano的经验总结
 
-
-
 #### svg-sprite-demo
 
 svg素材合成demo
+
+
+#### env
+手机环境快速判断工具
---
+++ env/env.js
@@ -0,0 +1,61 @@
+function getUa() {
+	var ua = "";
+
+	var navigator = window ? window.navigator : null;
+	ua = navigator.userAgent || "";
+
+	return ua;
+}
+
+var ua = getUa();
+
+function uaIncludes(str) {
+	return ua.indexOf(str) > -1;
+}
+
+function uaMatch(re) {
+	return ua.match(re);
+}
+
+function getIOSVersion() {
+	var version = ua.toLowerCase().match(/cpu iphone os (.*?) like mac os/);
+
+	if (version && Array.isArray(version) && version.length > 0 && version[1]) {
+		return version[1].replace(/_/gi, ".");
+	}
+
+	return "";
+}
+
+function getAndroidVersion() {
+	var version = ua.toLowerCase().match(/android [\d._]+/gi);
+
+	if (ua.toLowerCase().indexOf("android") > 0) {
+		return (version + "").replace(/[^0-9|_.]/gi, "").replace(/_/gi, ".");
+	}
+
+	return "";
+}
+
+var isMobile = !!uaMatch(/AppleWebKit.*Mobile.*/);
+var isIOS = !!uaMatch(/\(i[^;]+;( U;)? CPU.+Mac OS X/) || uaIncludes("iPhone");
+var isAndroid = uaIncludes("Android") || uaIncludes("Adr");
+var osName = isIOS ? 'ios' : 'android';
+var isIPhone = uaIncludes('iPhone');
+var isIPad = uaIncludes('iPad');
+var osVersion = isIOS ? getIOSVersion() : getAndroidVersion();
+
+var _default = {
+	isMobile,
+	isIOS,
+	isAndroid,
+	osName,
+	isIPhone,
+	isIPad,
+	osVersion,
+};
+
+console.log('env');
+console.log(_default);
+
+exports.default = _default;
