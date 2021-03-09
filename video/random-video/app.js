db55b6b57a317462a1ecb5bd90b66f084cb704f7
superzdd <superzdd@sina.com>
2021-03-10 00:18:40 +0800

    delete mp4 files

--- video/random-video/app.js
+++ video/random-video/app.js
@@ -1,5 +1,6 @@
 let appModel = {
-	videoSrc: ['../assets/video/vh.mp4', '../assets/video/vv.mp4', '../assets/video/video.mp4'],
+	videoSrc: ['http://alicdn.herdsric.com/herdsric-tool/vv.mp4',
+		'http://alicdn.herdsric.com/herdsric-tool/vh.mp4'],
 	videoIndex: 0,
 }
 
@@ -16,9 +17,7 @@ let appView = {
 			appModel.videoIndex = 0;
 		}
 
-		appView.$video.attr('src', appModel.videoSrc[appModel.videoIndex]);
-		// appView.$video.play();
-		// return appModel.videoIndex;
+		appView.$video.attr('src', appModel.videoSrc[appModel.videoIndex]);		
 	}
 }
 
